8173429f40d69979fdcca3023db1407663ae0fd9
Ahmed Assaleh <a-assaleh@outlook.com>
2021-03-05 19:54:57 +0200

    final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Pull Test
+This repo is a test for pulling the files from github (raw)
+
+## version 1
+In this method I wrote the full links in the index.txt, then pulled the files.
+this method is too simple and does not support directories.
+
+## version 2
+In this method I put the output of the command 'find -type (d/f)', into a index.json.
+make the directories and then pull the files into their directories.
---
+++ v2/src/pull.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+	echo '$1 -> directory'
+	exit 1
+fi
+
+# output directory
+output_dir="$1"
+mkdir -p "$output_dir"
+
+# hard coded link
+index_link="https://raw.githubusercontent.com/BYTE-LY/pull_test/master/method2/files/index.json"
+
+# temp file for the index
+index="$(mktemp)"
+
+# pull the index
+#wget -q --show-progress -O $index $index_link
+wget -q --show-progress -O $index $index_link
+
+## pull the files ##
+
+# make the dirs
+cat $index | jq -r '.directories[]' |
+	while read line ; do
+		mkdir -p $output_dir/$line
+	done
+
+# pull the files
+cat $index | jq -r '.files[]' |
+	while read line ; do
+		wget -q --show-progress -O "$output_dir$line" "https://raw.githubusercontent.com/BYTE-LY/pull_test/master/method2/files$line"
+		#echo "$output_dir$line -> https://raw.githubusercontent.com/BYTE-LY/pull_test/master/method2/files$line"
+
+	done
+
+# remove the temp file
+rm $index
