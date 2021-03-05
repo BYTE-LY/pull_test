6889ad02fb6aebb0bb6319f996d3b1a78254d003
Ahmed Assaleh <a-assaleh@outlook.com>
2021-03-05 18:21:40 +0200

    2

--- src/pull.sh
+++ src/pull.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+if [ -z "$1" ] ; then
+	echo '$1 -> directory'
+	exit 1
+fi
+
+# output directory
+output_dir="$1"
+mkdir -p "$output_dir"
+
+# hard coded link
 index_link="https://raw.githubusercontent.com/BYTE-LY/pull_test/master/files/index.txt"
+
+# temp file for the index
 index="$(mktemp)"
 
+# pull the index
+wget -q --show-progress -O $index $index_link
 
-# Download the index
-curl -o $index $index_link
+# pull the Files
+while read line ; do wget -q --show-progress -P "$output_dir" "$line" ; done < $index
+
+# remove the temp file
+rm $index
